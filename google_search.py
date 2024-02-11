6e9211956c455be48b9032411eb473eba65bb048
Ahnaf Azad <ahnaf2232@gmail.com>
2024-02-11 19:03:19 +0000

    added google search

--- google_search.py
+++ google_search.py
@@ -3,7 +3,6 @@ from googlesearch import search
 
 def google(query, num_results=5):
     try:
-        # Performing Google search with the specified query
         search_results = search(query, stop=num_results)
 
         # Printing the search results
@@ -11,15 +10,3 @@ def google(query, num_results=5):
             print(f"Result {i}: {result}")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-
-
-# Example usage
-if __name__ == "__main__":
-    # Query to search on Google
-    query = "what is the golden gate bridge made of"
-
-    # Number of search results to retrieve (default is 5)
-    num_results = 5
-
-    # Calling the function to perform Google search
-    google(query)
